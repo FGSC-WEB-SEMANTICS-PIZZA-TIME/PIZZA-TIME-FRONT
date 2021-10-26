915a94710afe3b9f2b2bf0e65f805adb0737745d
chihabhajji <blackpang23@gmail.com>
2021-10-26 12:13:09 +0100

    🍕

--- src/app/shared/_models/pagination-helper.model.ts
+++ src/app/shared/_models/pagination-helper.model.ts
@@ -1,5 +1,6 @@
 export class PaginationHelper{
   search: string = '';
+  type: string = '';
   dropdownValues: Map<string,string> = new Map<string, string>();
   sliderValues: Map<string,[min: number, max: number]> = new Map<string, [min: number, max: number]>();
   limit       = 10;
